105ecfd9bd6f25b1fa61b3684df5a94f8f690045
woriazzc <1925854083@qq.com>
2024-04-17 19:31:53 +0800

    update config of bpr@foursquare

--- run.sh
+++ run.sh
@@ -1,18 +1,29 @@
 # train teacher
+python -u main.py --dataset=foursquare --backbone=bpr --train_teacher --teacher embedding_dim=800 --cfg wd=0.01 --suffix teacher
+python -u main.py --dataset=foursquare --backbone=lightgcn --train_teacher --teacher embedding_dim=2000 --cfg wd=1e-3 --suffix teacher
+
+python -u main.py --dataset=citeulike --backbone=bpr --train_teacher --teacher embedding_dim=400 --cfg wd=1e-3 --suffix teacher
+python -u main.py --dataset=citeulike --backbone=lightgcn --train_teacher
+
 python -u main.py --dataset=gowalla --backbone=bpr --train_teacher
 python -u main.py --dataset=gowalla --backbone=lightgcn --train_teacher
 
-python -u main.py --dataset=foursquare --backbone=bpr --train_teacher
-python -u main.py --dataset=foursquare --backbone=lightgcn --train_teacher
+#from scratch
+python -u main.py --dataset=foursquare --backbone=bpr --train_teacher --teacher embedding_dim=10 --cfg wd=0.01 --suffix student
+python -u main.py --dataset=foursquare --backbone=lightgcn --train_teacher --teacher embedding_dim=10 --cfg wd=0.01 --suffix student
 
-python -u main.py --dataset=citeulike --backbone=bpr --train_teacher
-python -u main.py --dataset=citeulike --backbone=lightgcn --train_teacher
+python -u main.py --dataset=citeulike --backbone=bpr --train_teacher --teacher embedding_dim=20 --cfg wd=1e-3 --suffix student
 
 # KD
-python -u main.py --dataset=gowalla --backbone=bpr --model=rrd
+python -u main.py --dataset=citeulike --backbone=bpr --model=nkd
+
 
 python -u main.py --dataset=foursquare --backbone=bpr --model=de
 python -u main.py --dataset=foursquare --backbone=bpr --model=cpd
 python -u main.py --dataset=foursquare --backbone=bpr --model=nkd
+python -u main.py --dataset=foursquare --backbone=bpr --model=graphd
 
-python -u main.py --dataset=citeulike --backbone=bpr --model=de
+python -u main.py --dataset=foursquare --backbone=lightgcn --model=nkd
+
+
+python -u main.py --dataset=gowalla --backbone=bpr --model=rrd
