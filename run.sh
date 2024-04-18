31b6dd7c8ba68677f432be2c00b8a1ee6099982f
woriazzc <1925854083@qq.com>
2024-04-18 18:42:08 +0800

    update config lightgcn@citeulike

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 # train teacher
 python -u main.py --dataset=foursquare --backbone=bpr --train_teacher --teacher embedding_dim=800 --cfg wd=0.01 --suffix teacher
-python -u main.py --dataset=foursquare --backbone=lightgcn --train_teacher --teacher embedding_dim=2000 --cfg wd=1e-3 --suffix teacher
+python -u main.py --dataset=foursquare --backbone=lightgcn --train_teacher --teacher embedding_dim=2000 --cfg wd=1e-3 num_layers=1 --suffix teacher
 
 python -u main.py --dataset=citeulike --backbone=bpr --train_teacher --teacher embedding_dim=400 --cfg wd=1e-3 --suffix teacher
-python -u main.py --dataset=citeulike --backbone=lightgcn --train_teacher
+python -u main.py --dataset=citeulike --backbone=lightgcn --train_teacher --teacher embedding_dim=2000 --cfg wd=1e-7 num_layers=3 --suffix teacher
 
 python -u main.py --dataset=gowalla --backbone=bpr --train_teacher
 python -u main.py --dataset=gowalla --backbone=lightgcn --train_teacher
 
 #from scratch
 python -u main.py --dataset=foursquare --backbone=bpr --train_teacher --teacher embedding_dim=10 --cfg wd=0.01 --suffix student
-python -u main.py --dataset=foursquare --backbone=lightgcn --train_teacher --teacher embedding_dim=10 --cfg wd=0.01 --suffix student
+python -u main.py --dataset=foursquare --backbone=lightgcn --train_teacher --teacher embedding_dim=10 --cfg wd=0.01 num_layers=1 --suffix student
 
 python -u main.py --dataset=citeulike --backbone=bpr --train_teacher --teacher embedding_dim=20 --cfg wd=1e-3 --suffix student
+python -u main.py --dataset=citeulike --backbone=lightgcn --train_teacher --teacher embedding_dim=20 --cfg wd=1e-3 num_layers=3 --suffix student
 
 # KD
 python -u main.py --dataset=citeulike --backbone=bpr --model=nkd
