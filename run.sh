41a536d0f6b5860919274798f8b013bf031ab635
woriazzc <1925854083@qq.com>
2024-04-27 14:36:33 +0800

    fix cmd for training teacher

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 # train teacher
 python -u main.py --dataset=foursquare --backbone=bpr --train_teacher --teacher embedding_dim=800 --cfg wd=0.01 --suffix teacher
-python -u main.py --dataset=foursquare --backbone=lightgcn --train_teacher --teacher embedding_dim=2000 --cfg wd=1e-3 num_layers=1 --suffix teacher
+python -u main.py --dataset=foursquare --backbone=lightgcn --train_teacher --teacher embedding_dim=2000 num_layers=1 --cfg wd=1e-3 --suffix teacher
 
 python -u main.py --dataset=citeulike --backbone=bpr --train_teacher --teacher embedding_dim=400 --cfg wd=1e-3 --suffix teacher
-python -u main.py --dataset=citeulike --backbone=lightgcn --train_teacher --teacher embedding_dim=2000 --cfg wd=1e-7 num_layers=3 --suffix teacher
+python -u main.py --dataset=citeulike --backbone=lightgcn --train_teacher --teacher embedding_dim=2000 num_layers=3 --cfg wd=1e-7 --suffix teacher
 
 python -u main.py --dataset=gowalla --backbone=bpr --train_teacher
 python -u main.py --dataset=gowalla --backbone=lightgcn --train_teacher
 
 #from scratch
 python -u main.py --dataset=foursquare --backbone=bpr --model=scratch --student embedding_dim=10 --cfg wd=0.01 --suffix student
-python -u main.py --dataset=foursquare --backbone=lightgcn --model=scratch --student embedding_dim=10 --cfg wd=0.01 num_layers=1 --suffix student
+python -u main.py --dataset=foursquare --backbone=lightgcn --model=scratch --student embedding_dim=10 num_layers=1 --cfg wd=0.01 --suffix student
 
 python -u main.py --dataset=citeulike --backbone=bpr --model=scratch --student embedding_dim=20 --cfg wd=1e-3 --suffix student
-python -u main.py --dataset=citeulike --backbone=lightgcn --model=scratch --student embedding_dim=20 --cfg wd=1e-3 num_layers=3 --suffix student
+python -u main.py --dataset=citeulike --backbone=lightgcn --model=scratch --student embedding_dim=20 num_layers=3 --cfg wd=1e-3 --suffix student
 
 # KD
 # For HetComp, you need to pre-save teacher checkpoints through:
