cf4794d8c6b72084cd71fba8d20a69f096dac3f1
woriazzc <1925854083@qq.com>
2024-11-07 18:56:09 +0800

    add baseline DCD

--- modeling/KD/baseline.py
+++ modeling/KD/baseline.py
@@ -435,6 +435,88 @@ class RRD(BaseKD4Rec):
         return URRD_loss
 
 
+class DCD(BaseKD4Rec):
+    def __init__(self, args, teacher, student):
+        super().__init__(args, teacher, student)
+        self.K = args.dcd_K
+        self.T = args.dcd_T
+        self.mxK = args.dcd_mxK
+        self.ablation = args.ablation
+        self.tau = args.dcd_tau
+        self.T_topk = self.get_topk_dict()
+        self.T_rank = torch.arange(self.mxK).repeat(self.num_users, 1).cuda()
+
+    def get_topk_dict(self):
+        print('Generating Top-K dict...')
+        with torch.no_grad():
+            inter_mat = self.teacher.get_all_ratings()
+            train_pairs = self.dataset.train_pairs
+            # remove true interactions from topk_dict
+            inter_mat[train_pairs[:, 0], train_pairs[:, 1]] = -1e6
+            _, topk_dict = torch.topk(inter_mat, self.mxK, dim=-1)
+        return topk_dict
+    
+    def get_samples(self, batch_user):
+        underestimated_samples = torch.index_select(self.underestimated_items, 0, batch_user)
+        overestimated_samples = torch.index_select(self.overestimated_items, 0, batch_user)
+        return underestimated_samples, overestimated_samples
+
+    def do_something_in_each_epoch(self, epoch):
+        with torch.no_grad():
+            S_pred = self.student.get_all_ratings()
+            S_topk = torch.argsort(S_pred, descending=True, dim=-1)
+            S_rank = torch.argsort(S_topk, dim=-1)
+            S_rank = S_rank[torch.arange(len(S_rank)).unsqueeze(-1), self.T_topk]
+            diff = S_rank - self.T_rank
+            rank_diff = torch.maximum(torch.tanh(torch.maximum(diff / self.T, torch.tensor(0.))), torch.tensor(1e-5))
+            diff_inv = self.T_rank - S_rank
+            rank_diff_inv = torch.maximum(torch.tanh(torch.maximum(diff_inv / self.T, torch.tensor(0.))), torch.tensor(1e-5))
+
+            # sampling
+            underestimated_idx = torch.multinomial(rank_diff, self.K, replacement=False)
+            self.underestimated_items = self.T_topk[torch.arange(self.num_users).unsqueeze(-1), underestimated_idx]
+            overestimated_idx = torch.multinomial(rank_diff_inv, self.K, replacement=False)
+            self.overestimated_items = self.T_topk[torch.arange(self.num_users).unsqueeze(-1), overestimated_idx]
+    
+    def relaxed_ranking_loss(self, S1, S2):
+        S1 = torch.minimum(S1, torch.tensor(80., device=S1.device))     # This may help
+        S2 = torch.minimum(S2, torch.tensor(80., device=S2.device))
+
+        above = S1.sum(1, keepdims=True)
+
+        below1 = S1.flip(-1).exp().cumsum(1)    # exp() of interesting_prediction results in inf
+        below2 = S2.exp().sum(1, keepdims=True)
+
+        below = (below1 + below2).log().sum(1, keepdims=True)
+        
+        return -(above - below).sum()
+    
+    def ce_loss(self, logit_T, logit_S):
+        prob_T = torch.softmax(logit_T / self.tau, dim=-1)
+        loss = F.cross_entropy(logit_S / self.tau, prob_T, reduction='sum')
+        return loss
+
+    def get_loss(self, batch_user, batch_pos_item, batch_neg_item):
+        users = batch_user.unique()
+        underestimated_items, overestimated_items = self.get_samples(users)
+        underestimated_items = underestimated_items.type(torch.LongTensor).cuda()
+        overestimated_items = overestimated_items.type(torch.LongTensor).cuda()
+
+        underestimated_prediction = self.student.forward_multi_items(users, underestimated_items)
+        overestimated_prediction = self.student.forward_multi_items(users, overestimated_items)
+
+        if self.ablation:
+            underestimated_prediction_T = self.teacher.forward_multi_items(users, underestimated_items)
+            overestimated_prediction_T = self.student.forward_multi_items(users, overestimated_items)
+            prediction_T = torch.concat([underestimated_prediction_T, overestimated_prediction_T], dim=-1)
+            prediction_S = torch.concat([underestimated_prediction, overestimated_prediction], dim=-1)
+            loss = self.ce_loss(prediction_T, prediction_S)
+        else:
+            loss = self.relaxed_ranking_loss(underestimated_prediction, overestimated_prediction)
+
+        return loss
+
+
 class HTD(BaseKD4Rec):
     def __init__(self, args, teacher, student):
         super().__init__(args, teacher, student)
