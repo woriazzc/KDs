77466f5721fe1c9e7a3ae2a9c44eb4e0f3b485a4
woriazzc <1925854083@qq.com>
2025-02-26 21:11:48 +0800

    Update FreqMM to common

--- modeling/KD/mm/playground.py
+++ modeling/KD/mm/playground.py
@@ -10,7 +10,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from ..utils import Projector, pca, load_pkls, dump_pkls, self_loop_graph
+from ..utils import Projector, pca, load_pkls, dump_pkls, self_loop_graph, sym_norm_graph
 from ..base_model import BaseKD4MM
 
 
@@ -37,39 +37,35 @@ class FreqMM(BaseKD4MM):
                                             for m in self.modality_names})
 
         all_u, all_i = self.teacher.get_all_embedding()
-        self.nearestK_u, self.nearestK_i = self.get_nearest_K(all_u, all_i, self.K)
-        self.Graph_u = self.construct_knn_graph(self.nearestK_u)
-        self.Graph_i = self.construct_knn_graph(self.nearestK_i)
+        self.Graph_u = self.construct_knn_graph(all_u, self.K, "user")
+        self.Graph_i = self.construct_knn_graph(all_i, self.K, "item")
         self.all_T_u, self.all_T_i = self.teacher.get_all_embedding()
         self.all_T_mm = self.teacher.get_item_modality_embedding(self.teacher.item_list)
     
     def get_params_to_update(self):
         return [{"params": [param for param in self.parameters() if param.requires_grad], 'lr': self.args.lr, 'weight_decay': self.args.wd}]
 
+    @torch.no_grad()
     def _KNN(self, embs, K):
-        with torch.no_grad():
-            embs = pca(embs, 150)
-            topk_indices = knn(embs, embs, k=K+1)[1].reshape(-1, K + 1)
+        embs = pca(embs, 150)
+        topk_indices = knn(embs, embs, k=K+1)[1].reshape(-1, K + 1)
         return topk_indices[:, 1:].cuda()
-
-    def get_nearest_K(self, all_u, all_i, K):
-        f_nearestK_u = os.path.join("modeling", "KD", "crafts", self.args.dataset, self.args.T_backbone, self.model_name, f"nearest_{K}_u.pkl")
-        f_nearestK_i = os.path.join("modeling", "KD", "crafts", self.args.dataset, self.args.T_backbone, self.model_name, f"nearest_{K}_i.pkl")
-        sucflg, nearestK_u, nearestK_i = load_pkls(f_nearestK_u, f_nearestK_i)
-        if not sucflg:
-            nearestK_u = self._KNN(all_u, K)
-            nearestK_i = self._KNN(all_i, K)
-            dump_pkls((nearestK_u, f_nearestK_u), (nearestK_i, f_nearestK_i))
-        return nearestK_u, nearestK_i
     
-    def construct_knn_graph(self, neighbor_id):
-        N, K = neighbor_id.shape
+    def construct_knn_graph(self, all_embed, K, name):
+        f_graph = os.path.join("modeling", "KD", "crafts", self.args.dataset, self.args.T_backbone, self.model_name, f"Graph_{name}_{K}.pkl")
+        sucflg, Graph = load_pkls(f_graph)
+        if sucflg: return Graph.cuda()
+        N = all_embed.shape[0]
+        nearestK = self._KNN(all_embed, K)
         row = torch.arange(N).repeat(K, 1).T.reshape(-1).cuda()
-        col = neighbor_id.reshape(-1)
+        col = nearestK.reshape(-1)
         index = torch.stack([row, col])
-        data = torch.ones(index.size(-1)).cuda() / K
+        data = torch.ones(index.size(-1)).cuda()
         Graph = torch.sparse_coo_tensor(index, data, torch.Size([N, N]), dtype=torch.float)
+        Graph = (Graph + Graph.T) / 2
         Graph = Graph.coalesce()
+        Graph = sym_norm_graph(Graph)
+        dump_pkls((Graph, f_graph))
         return Graph.cuda()
 
     def _dropout_graph(self, Graph):
@@ -81,55 +77,25 @@ class FreqMM(BaseKD4MM):
         random_index = random_index.int().bool()
         index = index[random_index]
         values = values[random_index] / self.keep_prob
+        droped_Graph = torch.sparse_coo_tensor(index.t(), values, size, dtype=torch.float)
+        return droped_Graph
 
-        self_loop_idx = torch.stack([torch.arange(size[0]), torch.arange(size[0])], dim=1).cuda()
-        self_loop_data = torch.ones(self_loop_idx.size(0)).cuda()
-
-        rndS = random.random() * self.alpha1 * 2
-        rndT = random.random() * self.alpha1 * 2
-        valuesS = torch.cat([values * rndS, self_loop_data * (1. - rndS)])
-        valuesT = torch.cat([values * rndT, self_loop_data * (1. - rndT)])
-
-        index = torch.cat([index, self_loop_idx], dim=0)
-
-        droped_GraphS = torch.sparse_coo_tensor(index.t(), valuesS, size, dtype=torch.float)
-        droped_GraphT = torch.sparse_coo_tensor(index.t(), valuesT, size, dtype=torch.float)
-        return droped_GraphS, droped_GraphT
+    def generate_filter(self, Adj, alpha):
+        droped_Adj = self._dropout_graph(Adj)
+        rndS = random.random() * alpha * 2
+        rndT = random.random() * alpha * 2
+        self_loop = self_loop_graph(droped_Adj.shape[0]).cuda()
+        H_S = droped_Adj * rndS + self_loop * (1. - rndS)
+        H_T = droped_Adj * rndT + self_loop * (1. - rndT)
+        return H_S, H_T
     
-    def _dropout_graph_mm(self, Graph):
-        size = Graph.size()
-        assert size[0] == size[1]
-        index = Graph.indices().t()
-        values = Graph.values()
-        random_index = torch.rand(len(values)) + self.keep_prob
-        random_index = random_index.int().bool()
-        index = index[random_index]
-        values = values[random_index] / self.keep_prob
-
-        self_loop_idx = torch.stack([torch.arange(size[0]), torch.arange(size[0])], dim=1).cuda()
-        self_loop_data = torch.ones(self_loop_idx.size(0)).cuda()
-
-        rndS = random.random() * self.alpha2 * 2
-        rndT = random.random() * self.alpha2 * 2
-        # valuesS = torch.cat([values * (-rndS), self_loop_data * (1. - rndS)])
-        # valuesT = torch.cat([values * (-rndT), self_loop_data * (1. - rndT)])
-        valuesS = torch.cat([values * rndS, self_loop_data * (1. - rndS)])
-        valuesT = torch.cat([values * rndT, self_loop_data * (1. - rndT)])
-
-        index = torch.cat([index, self_loop_idx], dim=0)
-
-        droped_GraphS = torch.sparse_coo_tensor(index.t(), valuesS, size, dtype=torch.float)
-        droped_GraphT = torch.sparse_coo_tensor(index.t(), valuesT, size, dtype=torch.float)
-        return droped_GraphS, droped_GraphT
-
     def freq_loss(self, batch_entity, all_S, all_T, GraphS, GraphT, projector):
         S = torch.sparse.mm(GraphS, all_S)
         T = torch.sparse.mm(GraphT, all_T)
         T_feas = T[batch_entity]
         S_feas = S[batch_entity]
         S_feas = projector(S_feas)
-        norm_T = T_feas.pow(2).sum(-1, keepdim=True).pow(1. / 2)
-        T_feas = T_feas.div(norm_T)
+        T_feas = F.normalize(T_feas, p=2, dim=-1)
         cos_theta = (T_feas * S_feas).sum(-1, keepdim=True)
         G_diff = 1. - cos_theta
         loss = G_diff.sum()
@@ -139,18 +105,18 @@ class FreqMM(BaseKD4MM):
         all_S = self.student.user_emb.weight
         all_T = self.all_T_u
         projector = self.projector_u
-        droped_GraphS, droped_GraphT = self._dropout_graph(self.Graph_u)
-        loss = self.freq_loss(batch_entity, all_S, all_T, droped_GraphS, droped_GraphT, projector)
+        H_S_u, H_T_u = self.generate_filter(self.Graph_u, self.alpha1)
+        loss = self.freq_loss(batch_entity, all_S, all_T, H_S_u, H_T_u, projector)
         return loss
     
     def get_item_loss(self, batch_entity):
-        droped_GraphS, droped_GraphT = self._dropout_graph(self.Graph_i)
         all_T = self.all_T_i
         all_S_id = self.student.item_emb.weight
-        loss = self.freq_loss(batch_entity, all_S_id, all_T, droped_GraphS, droped_GraphT, self.projector_i)
-        droped_GraphS, droped_GraphT = self._dropout_graph_mm(self.Graph_i)
+        H_S_i, H_T_i = self.generate_filter(self.Graph_i, self.alpha1)
+        loss = self.freq_loss(batch_entity, all_S_id, all_T, H_S_i, H_T_i, self.projector_i)
         for m in self.all_T_mm:
-            loss_m = self.freq_loss(batch_entity, all_S_id, self.all_T_mm[m], droped_GraphS, droped_GraphT, self.projector_mm[m])
+            H_S_mm, H_T_mm = self.generate_filter(self.Graph_i, self.alpha2)
+            loss_m = self.freq_loss(batch_entity, all_S_id, self.all_T_mm[m], H_S_mm, H_T_mm, self.projector_mm[m])
             loss += self.beta * loss_m
         return loss
 
